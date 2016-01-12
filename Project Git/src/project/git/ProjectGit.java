49c8dd3aee8e591d0ce1f3078adf2ca8e8ded070
dam1 <dam1@1DAM-06.IES.QUEVEDO-AD>
2016-01-12 11:01:14 +0100

    Segundo cambio

--- Project Git/src/project/git/ProjectGit.java
+++ Project Git/src/project/git/ProjectGit.java
@@ -16,6 +16,7 @@ public class ProjectGit {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("modafacas");
     }
     
 }
